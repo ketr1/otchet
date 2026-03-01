ecfbcd71cb1d235aa66831e0c96a79eda944dc36
ketr1 <ketpodpischik@gmail.com>
2026-03-01 17:42:14 +0300

    added tests

--- shop/tests.py
+++ shop/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
 
-# Create your tests here.
+class PagesTest(TestCase):
+    def test_login_page_status_code(self):
+        response = self.client.get("/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_products_page_status_code(self):
+        response = self.client.get("/products/")
+        self.assertIn(response.status_code, (200, 302))
